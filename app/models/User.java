bb78211dade03fbc4115ccc1f0291d6f36394c4d
whitney <whitneyrzoller@gmail.com>
2011-09-24 18:38:04 -0400

    proper relationships

--- app/models/User.java
+++ app/models/User.java
@@ -8,17 +8,21 @@ import play.db.jpa.Model;
 @Table(name = "`users`")
 public class User extends Model {
      
+	@Id
+	@Column(name="id")
+	public long id;
     public String email;
     public String first_name;
     public String last_name;
+	@ManyToMany
+	@JoinTable(name="users_listings",
+		joinColumns={@JoinColumn(name="user_id", referencedColumnName="id")},
+		inverseJoinColumns={@JoinColumn(name="rental_listing_id", referencedColumnName="id")})
+	private List<RentalListing> rentalListings;
     
     public User(String email, String first_name, String last_name) {
         this.email = email;
         this.first_name = first_name;
         this.last_name = last_name;
     }
-
-    // inverse side of the relation
-    //@ManyToMany(mappedBy = "users")
-    //public List<Neighborhood> neighborhoods;
 }
