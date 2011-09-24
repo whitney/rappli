aac9f924d442a97e0fa13ff7e6be9ddae8d53eb6
whitney <whitneyrzoller@gmail.com>
2011-09-24 18:52:08 -0400

    unfucking relations

--- app/models/User.java
+++ app/models/User.java
@@ -14,11 +14,13 @@ public class User extends Model {
     public String email;
     public String first_name;
     public String last_name;
+	/*
 	@ManyToMany
 	@JoinTable(name="users_listings",
 		joinColumns={@JoinColumn(name="user_id", referencedColumnName="id")},
 		inverseJoinColumns={@JoinColumn(name="rental_listing_id", referencedColumnName="id")})
 	private List<RentalListing> rentalListings;
+	*/
     
     public User(String email, String first_name, String last_name) {
         this.email = email;
