406fc596359d44192670b42febdc6ee0ae60810d
Angel <perangel@gmail.com>
2011-10-01 21:53:08 -0400

    Cleaning up

--- app/controllers/Craigslist.java
+++ app/controllers/Craigslist.java
@@ -1,29 +1,64 @@
 package controllers;
 import java.io.IOException;
+import play.mvc.results.*;
 
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class Craigslist extends Application {
 
-	public static void submitUrl(String url) {
-		Document listing = null;
-		try {
-			listing = Jsoup.connect(url).get();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-				
-		// Find the link to the owner's email
-		Elements replyToLink = listing.select("br + a[href~=mailto]");
-		String mailto = replyToLink.text();
-		String mailtoHref = replyToLink.attr("href");
+public class Craigslist extends Application {
+	public static void processListing(String url) {
+		Document doc = null;
 		
-		Elements googleMapLink = null;		
-		googleMapLink = listing.select("a:contains(google map)");
-		String googleMapHref = googleMapLink.attr("href");
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			badRequest();
+		} catch (IOException e) {
+			badRequest();
+		}
+		
+		// Example: http://newyork.craigslist.org/brk/roo/2625483628.html
+		url = url.replaceAll(".html", "");
+		String[] urlFragments = url.split("/");
+		long postId = Long.parseLong(
+				urlFragments[urlFragments.length - 1]);
+		System.out.println(postId);
+		
+		// Extract post title
+		String postTitle = doc.select("h2").text();
+		System.out.println(postTitle);
+		
+		// Extract post date
+		// Elements postDateElement = doc.select("h2 + hr");
+		// System.out.println(postDateElement);
+		// Element headerRow = postDateElement.first();
+		// System.out.println(headerRow.firstElementSibling());
+		// Element headerRow = postDateElement.first();
+		// String postDate = headerRow.firstElementSibling().text();
+		// System.out.println(postDate);
+		
+		
+		// Extract reply-to email
+		Elements replyToLink = doc.select("br + a[href~=mailto]");
+		String replyToEmail = replyToLink.text();
+		System.out.println(replyToEmail);
+		String mailToHref = replyToLink.attr("href");
+		System.out.println(mailToHref);
+		// Extract Google maps link
+		Elements googleMapLink = null;
+		googleMapLink = doc.select("a:contains(google map)");
+		if (googleMapLink != null) {
+			String googleMapHref = googleMapLink.attr("href");
+		}
+	}
+
+	
+	public static void index() {
+		render();
 	}
 }
+
