7aed9a5c8c47ceacce355b2cb3c3204be08eaa39
whitney <whitneyrzoller@gmail.com>
2011-10-09 09:46:23 -0400

    adding password to the users table

--- db/migrate/rappli/1.up.create_users.sql
+++ db/migrate/rappli/1.up.create_users.sql
@@ -4,6 +4,8 @@ CREATE TABLE users (
 	`email` VARCHAR(255) DEFAULT NULL, 
 	`first_name` VARCHAR(255) DEFAULT NULL, 
 	`last_name` VARCHAR(255) DEFAULT NULL, 
+	`password` BINARY(60) DEFAULT NULL, 
+	`activated` TINYINT(1) DEFAULT NULL, 
 	`created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00', 
 	`updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
 	`creator_id` int(11) DEFAULT NULL, 
