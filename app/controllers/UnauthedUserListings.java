41b2536f5599ffbb6603aae3936f501f17770cc9
whitney <whitneyrzoller@gmail.com>
2011-10-23 18:22:32 -0400

    creating and fetching unauthed-users listings

--- app/controllers/UnauthedUserListings.java
+++ app/controllers/UnauthedUserListings.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+
 import models.UnauthedUserListing;
 import models.RentalListing;
+
 import play.Logger;
 import play.data.validation.Required;
 
@@ -15,8 +20,24 @@ public class UnauthedUserListings extends Application {
 		rentalListing.save();
 		
 		String ipv4 = request.remoteAddress;
-		
-		//UnauthedUserListing unauthedListing = new UnauthedUserListing(ipv4, rentalListing);
-		//unauthedListing.save();
+		try {
+			InetAddress addr = InetAddress.getByName(ipv4);
+			UnauthedUserListing unauthedListing = new UnauthedUserListing(addr, rentalListing);
+			unauthedListing.save();
+		} catch(UnknownHostException e) {
+			Logger.error(">>> Invalid ipv4 address: " + ipv4);
+		}
+	}
+	
+	public static void get() {
+		String ipv4 = request.remoteAddress;
+		List<UnauthedUserListing> listings = null;
+		try {
+			InetAddress addr = InetAddress.getByName(ipv4);
+			listings = UnauthedUserListing.findByIp(addr);
+			renderJSON(listings);
+		} catch(UnknownHostException e) {
+			Logger.error(">>> Invalid ipv4 address: " + ipv4);
+		}	
 	}
 }
